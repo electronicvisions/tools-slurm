1b7241917912f47bc5617cac85ee9095f452f6d4
JulianGoeltz <goeltz@stud.uni-heidelberg.de>
2021-02-03 12:19:31 +0000

    Listing licences currently either pending or running.
    * first listed are runnning, then pending, and in colorful output
    * only the cube setups are looked at
    * if arguments are given (other than -h or --help), they are used as arguments
      for grep on the jobinfo, and in case they fit the job is skipped. This is
      useful to still see important info if someone is spamming the queue
    * jobs on environment variable {LLSS} (ListLicencesSpecialSetup, for setups
      that are of special interest) are highlighted; can be regex. Example:
      $LLSS=W66 or $LLSS="(W66|W67)"
    * vis_jenkin on setup W62F is shortened to reduce clutter
    * if job_user and USER agree, special magic is done to infer info about the job
    
    Change-Id: Id7c77aea4d2a279a20d0dfa722b0f3c0e49e4754

---
+++ tests/shell/test_listLicences.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail
+shopt -s expand_aliases
+
+source $MODULESHOME/init/bash
+module load slurm-singularity/current
+
+# Use perl for shebang-aware execution of the (not yet executable) script prior to installation
+alias listLicences="perl ${WAF_TOPLEVEL}/tools-slurm/src/shell/listLicences"
+
+# test ordinary way
+listLicences
+
+# test help
+listLicences --help
+
+# test skipping a user
+listLicences --skip jgoeltz
+
+# test with a special setup in regex
+listLicences --colour W67
