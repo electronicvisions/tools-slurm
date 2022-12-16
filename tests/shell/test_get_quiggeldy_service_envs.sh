934412d06eb4755007022501c8ff0d64a8bfa6b1
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2022-12-16 15:51:50 +0000

    Remove explicit loading of slurm-singularity module
    Change-Id: I32322e36070686625af711c391014eeb5f227c7c

--- tests/shell/test_get_quiggeldy_service_envs.sh
+++ tests/shell/test_get_quiggeldy_service_envs.sh
@@ -2,9 +2,6 @@
 set -euo pipefail
 shopt -s expand_aliases
 
-source $MODULESHOME/init/bash
-module load slurm-singularity/current
-
 # Use perl for shebang-aware execution of the (not yet executable) script prior to installation
 alias quiggeldy_envs="perl ${WAF_TOPLEVEL}/tools-slurm/src/shell/quiggeldy_envs"
 
